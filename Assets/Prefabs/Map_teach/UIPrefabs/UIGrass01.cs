67b61512e63f74e45fd44cc716151df7c5048bca
pzl <952982606@qq.com>
2020-07-02 20:45:01 +0800

    the tree and other things

--- Assets/Prefabs/Map_teach/UIPrefabs/UIGrass01.cs
+++ Assets/Prefabs/Map_teach/UIPrefabs/UIGrass01.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class UIGrass01 : MonoBehaviour
 {
+    public bool isShow;
+
+    public float showTime;
+    private float showTimer;
+
+    public string info;
+
+    public GameObject spark;
+    public GameObject[] sparkPlace;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isShow = false;
+        showTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        checkAndShow();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            isShow = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            isShow = false ;
+        }
+    }
+
+    private void checkAndShow()
+    {
         
+
     }
 }

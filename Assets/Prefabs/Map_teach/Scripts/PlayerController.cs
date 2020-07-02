71caf76e45337013561e63dbff791742542608d0
pzl <952982606@qq.com>
2020-07-02 11:00:17 +0800

    try cinemachine

---
+++ Assets/Prefabs/Map_teach/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+
+    public float moveSpeed;//移动速度
+    public float jumpForce;//跳跃高度
+
+    public static PlayerController instance;
+
+    private float bornPosX;//出生点的X坐标，用来平移环境图
+
+    
+
+    void Start()
+    {
+        instance = this;
+
+        bornPosX = this.transform.position.x;
+    }
+
+    void FixedUpdate()
+    {
+        //角色移动
+        Movement();
+
+        //环境分层偏移
+        EnvironmentBias();
+    }
+
+    private void Movement()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float faceDirectiong = Input.GetAxisRaw("Horizontal");
+
+        //水平移动
+        if (horizontalMove != 0)
+        {
+            rigid.velocity = new Vector2(horizontalMove * moveSpeed * Time.deltaTime, rigid.velocity.y);
+        }
+
+        //跳跃
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, jumpForce * Time.deltaTime);
+        }
+    }
+
+    private void EnvironmentBias()
+    {
+
+    }
+}
---
+++ Assets/Prefabs/Map_teach/Scripts/ViewPointWithCursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewPointWithCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //根据鼠标的位置进行视角的偏移
+        this.transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    }
+}

4bb208a2fcc1a71622f5bf221677be7ea7731c5a
胡勇 <710039828@qq.com>
2020-07-03 09:59:01 +0800

    整合

--- Assets/Prefabs/Map_teach/Scripts/PlayerController.cs
+++ Assets/Prefabs/Map_teach/Scripts/PlayerController.cs
@@ -4,6 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Start is called before the first frame update
+    [Header("移动")]
+    [SerializeField] float WalkSpeed;
+    [SerializeField] float AccelerateTime;
+    [SerializeField] float DecelerateTime;
+    [SerializeField] Vector2 InputOffset;
+    [SerializeField] bool CanMove = true;
+
+    [Header("跳跃")]
+    [SerializeField] float JumpingSpeed;
+    [SerializeField] float FallMultiplier;
+    [SerializeField] float LowJumpMultiplier;
+    [SerializeField] bool CanJump = true;
+
+
+    [Header("触地判定")]
+    [SerializeField] private Vector2 PointOffset;
+    [SerializeField] private Vector2 Size;
+    [SerializeField] private LayerMask GroundLayerMask;
+    [SerializeField] bool GravityModifier = true;
+
+
+    Vector2 dir;
+
+    Rigidbody2D Rig;
+    Animator Anim;
+
+    [SerializeField] bool isMoving;
+    [SerializeField] bool isOnGround;
+    [SerializeField] float velocityX;
+    [SerializeField] bool isJumping;
+    [SerializeField] bool GravitySwitch = true;
+
+    [SerializeField] bool isAbleToCtrl = true;
+    [SerializeField] bool isDead = false;
+
     public Rigidbody2D rigid;
     public Collider2D clid;
 
@@ -28,6 +64,14 @@ public class PlayerController : MonoBehaviour
     //背景层次
     public Transform[] EnvirLayer;
 
+    void Awake()
+    {
+        Rig = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
+        Rig.velocity = Vector2.zero;
+    }
+
+
     void Start()
     {
         instance = this;
@@ -43,24 +87,205 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("Jump") && jumpCount > 0)
-        {
-            jumpPressed = true;
-        }
+        //if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        //{
+        //    jumpPressed = true;
+        //}
     }
 
     void FixedUpdate()
     {
-        //角色左右移动
-        Movement();
+        ////角色左右移动
+        //Movement();
 
-        //跳跃
-        //判断是否在地面
-        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
-        Jump();
+        ////跳跃
+        ////判断是否在地面
+        //isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
+        //Jump();
+
+
+        if (isAbleToCtrl)
+        {
+
+            isOnGround = OnGround();
+
+            #region 移动
+            if (CanMove)
+            {
+                if (Input.GetAxisRaw("Horizontal") > InputOffset.x)
+                {
+                    if (Rig.velocity.x < WalkSpeed * Time.fixedDeltaTime * 60)
+                        Rig.velocity = new Vector2(Mathf.SmoothDamp(Rig.velocity.x,
+                            WalkSpeed * Time.fixedDeltaTime * 60,
+                            ref velocityX, AccelerateTime), Rig.velocity.y);
+                    isMoving = true;
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    Anim.SetBool("run", true);
+                }
+
+                else if (Input.GetAxisRaw("Horizontal") < InputOffset.x * -1)
+                {
+                    if (Rig.velocity.x > WalkSpeed * Time.fixedDeltaTime * 60 * -1)
+                        Rig.velocity = new Vector2(Mathf.SmoothDamp(Rig.velocity.x,
+                            WalkSpeed * Time.fixedDeltaTime * 60 * -1,
+                            ref velocityX, AccelerateTime), Rig.velocity.y);
+                    isMoving = true;
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                    Anim.SetBool("run", true);
+                }
+
+                else
+                {
+                    isMoving = false;
+                    Rig.velocity = new Vector2(Mathf.SmoothDamp(Rig.velocity.x,
+                        0, ref velocityX,
+                        DecelerateTime), Rig.velocity.y);
+                    if (Rig.velocity.x <= 0.01f)
+                    {
+                        Rig.velocity = new Vector2(0, Rig.velocity.y);
+                    }
+                    Anim.SetBool("run", false);
+                }
+            }
+            #endregion
+
+            #region 跳跃
+            if (CanJump)
+            {
+                if (Input.GetAxisRaw("Jump") == 1 && !isJumping)
+                {
+                    Rig.velocity = new Vector2(Rig.velocity.x, JumpingSpeed);
+                    isJumping = true;
+                    Anim.SetTrigger("takeof");
+                }
+
+                if (isOnGround && Input.GetAxisRaw("Jump") == 0)
+                {
+                    isJumping = false;
+
+                }
+                if (isOnGround)
+                {
+                    Anim.SetBool("jump", false);
+                    Anim.SetBool("jumpup", false);
+                    Anim.SetBool("jumpdown", false);
+                }
+                else
+                {
+                    Anim.SetBool("jump", true);
+                }
+            }
+            #endregion
+
+            #region 重力控制器
+            if (GravityModifier)
+            {
+                if (Rig.velocity.y < 0)//玩家下坠
+                {
+                    Anim.SetBool("jumpdown", true);
+                    Rig.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplier - 1) * Time.fixedDeltaTime;//加速下坠
+                }
+
+                //if (prop1equip)
+                //{
+                //if (Rig.velocity.y > 0 && Input.GetAxis("Jump") != 1)//玩家上升，并且没有按下跳跃键
+                //{
+                //    Anim.SetBool("jumpup", true);
+                //    Rig.velocity += Vector2.up * Physics2D.gravity.y * (LowJumpMultiplier - 1) * Time.fixedDeltaTime;//减缓上升
+                //}
+                //}
+                //else
+                //{
+                if (Rig.velocity.y > 0)
+                {
+                    Anim.SetBool("jumpup", true);
+                }
+
+                Rig.velocity += Vector2.up * Physics2D.gravity.y * (LowJumpMultiplier - 1) * Time.fixedDeltaTime;//减缓上升
+                                                                                                                 //}
+
+            }
+            #endregion
+
+            //#region 冲刺
+            //if (prop2equip)
+            //{
+            //    if (Input.GetAxisRaw("Dash") == 1 && !WasDashed)
+            //    {
+            //        WasDashed = true;
+            //        dir = GetDir();
+            //        StartCoroutine(Dash());//使用
+            //                               //将玩家当前所有动量清零
+            //        Rig.velocity = Vector2.zero;
+            //        //施加一个力，让玩家飞出去
+            //        Rig.velocity += dir.normalized * DragForce;
+
+            //    }
+
+            //    if (isOnGround && Input.GetAxisRaw("Dash") == 0)
+            //    {
+            //        WasDashed = false;
+            //    }
+            //}
+            //#endregion
+        }
 
         //环境分层偏移
         EnvironmentBias();
+    }
+
+
+    public Vector2 GetDir()
+    {
+        Vector2 tempDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (tempDir.x == 0 && tempDir.y == 0)
+        {
+            tempDir.x = transform.eulerAngles.y == 0 ? 1 : -1;
+        }
+
+        return tempDir;
+    }
+
+    bool OnGround()
+    {
+        Collider2D Coll = Physics2D.OverlapBox((Vector2)transform.position + PointOffset, Size, 0, GroundLayerMask);
+        bool isbool = Coll != null ? true : false;
+        return isbool;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube((Vector2)transform.position + PointOffset, Size);
+    }
+
+
+
+
+
+
+
+    void Death()
+    {
+        Vector3 deathPoint = transform.position;
+
+        //重置
+        CanMove = true;
+        CanJump = true;
+        GravityModifier = true;
+        Rig.gravityScale = 1;
+        //Anim.SetBool("isjump", false);
+        Anim.SetBool("isrunning", false);
+        Rig.velocity = Vector2.zero;
+        //设置位置
+
+        //Vector3 rebrithPoint = GameCtrl.gc.GetRebrithPoint(deathPoint);
+        //GameCtrl.gc.Warp(transform, rebrithPoint);
+
+        isAbleToCtrl = true;
+        isDead = false;
+
+
     }
 
     private void Movement()

bfaf1e3c98b801dc4f4e7afe33963766c6f7de3a
mooncakeLuo <5521935002@qq.com>
2020-07-15 22:14:06 +0800

    FairyGUI

---
+++ Assets/UIButtonLayout.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class UIButtonLayout : MonoBehaviour
+{
+    public UIPanel panel;
+
+    private GComponent ui;
+    private Transition tranStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GComponent ui = panel.ui;
+        tranStart = ui.GetTransition("start");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            tranStart.Play();
+        }
+    }
+}

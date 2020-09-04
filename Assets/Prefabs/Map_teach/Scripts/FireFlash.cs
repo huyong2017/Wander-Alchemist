8aaba2544aec321963cdc97c132a023710a2d477
pzl <952982606@qq.com>
2020-09-04 14:54:50 +0800

    adjust fire

---
+++ Assets/Prefabs/Map_teach/Scripts/FireFlash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class FireFlash : MonoBehaviour
+{
+    Light2D mlight;
+    void Start()
+    {
+        mlight = this.GetComponent<Light2D>();
+    }
+
+    float weight;
+    void Update()
+    {
+        weight =  0.5f * Mathf.Sin(10 * Time.time) + 1;
+        mlight.intensity = weight;
+        this.transform.Translate(new Vector3(0.05f * Mathf.Sin(Time.time),0,0));
+    }
+}

3785ef045c6fd0c815e95a04bff141dd0abb2c8c
胡勇 <710039828@qq.com>
2020-09-11 15:39:21 +0800

    bag2

---
+++ Assets/Scripts/BagManage/EnergyItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Energy", menuName = "Energy/New Energy")]
+
+public class EnergyItem : ScriptableObject
+{
+    string name;
+    int num;
+    int id;
+    // Start is called before the first frame update
+}
